7b3d2cc2fa4074c3165dfcf6bdf47451f9570fda
Denis Mukhin <mukhin.denis88@gmail.com>
2022-11-13 20:50:45 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -16,6 +16,39 @@ class FindLoopTest {
     }
 
     @Test
+    void whenDiaposonHasSomeEqualeElementThen3() {
+        int[] data = new int[] {5, 10, 2, 4, 8, 4, 14, 4, 3, 21, 16};
+        int el = 4;
+        int start = 1;
+        int finish = 8;
+        int result = FindLoop.indexRange(data, el, start, finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenDiapasonHas2Then3() {
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexRange(data, el, start, finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenDiapasonHasNot8ThenMinus1() {
+        int[] data = new int[] {4, 14, 4, 3, 21, 16};
+        int el = 8;
+        int start = 0;
+        int finish = 2;
+        int result = FindLoop.indexRange(data, el, start, finish);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
     void whenArrayHasNotThenMinus1() {
         int[] data = {3, 7, 0};
         int el = 9;
