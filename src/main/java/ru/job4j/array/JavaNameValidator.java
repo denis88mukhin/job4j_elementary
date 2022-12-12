c42e16f23322d6b7f39f18cdfdd651a4b48e0a59
Denis Mukhin <mukhin.denis88@gmail.com>
2022-12-12 23:19:45 +0300

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,42 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        boolean valid = true;
+        if (name.isEmpty() || isUpperLatinLetter(name.codePointAt(0)) || Character.isDigit(name.codePointAt(0))) {
+            valid = false;
+        } else {
+            for (int i = 1; i < name.length(); i++) {
+                int code = name.codePointAt(i);
+                if (!isUpperLatinLetter(code) && !isLowerLatinLetter(code) && !isSpecialSymbol(code) && !Character.isDigit(code)) {
+                    valid = false;
+                }
+            }
+        }
+        return valid;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        boolean valid = false;
+        if (code == 36 || code == 95) {
+            valid = true;
+        }
+        return valid;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        boolean valid = true;
+        if (code < 65 || code > 90) {
+            valid = false;
+        }
+        return valid;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        boolean valid = true;
+        if (code < 97 || code > 122) {
+            valid = false;
+        }
+        return valid;
+    }
+}
---
+++ src/test/java/ru/job4j/array/JavaNameValidatorTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import static ru.job4j.array.JavaNameValidator.*;
+
+class JavaNameValidatorTest {
+
+    @Test
+    void whenEmptyNameInvalid() {
+        assertThat(isNameValid("")).isFalse();
+    }
+
+    @Test
+    void whenFullLatinValid() {
+        assertThat(isNameValid("first")).isTrue();
+    }
+
+    @Test
+    void whenFullLatAndSomeUpperCaseLettersValid() {
+        assertThat(isNameValid("fIRST")).isTrue();
+    }
+
+    @Test
+    void whenLastNumberValid() {
+        assertThat(isNameValid("first1")).isTrue();
+    }
+
+    @Test
+    void whenLatWirhSpecialSymbolUnderValid() {
+        assertThat(isNameValid("first_user")).isTrue();
+    }
+
+    @Test
+    void whenNumberInvalid() {
+        assertThat(isNameValid("123")).isFalse();
+    }
+
+    @Test
+    void whenFirstLatinUpperCaseIsInvalid() {
+        assertThat(isNameValid("First")).isFalse();
+    }
+
+    @Test
+    void whenLatNumberAndTwoSpecialSymbolIsUnderValid() {
+        assertThat(isNameValid("fir$t_u$er_1")).isTrue();
+    }
+}
